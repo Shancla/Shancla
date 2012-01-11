a5c2e366d04f3b55dca6af66937bf1acabfe282f
migueldemalaga <migueldemalaga@gmail.com>
2012-01-11 02:00:19 +0100

    Esquema de script de busqueda INCOMPLETO!!!

--- shancla/busqueda_script.php
+++ shancla/busqueda_script.php
@@ -1,6 +1,10 @@
 <?php
-//  VALIDACION DE DATOS DEL FORMULARIO
-//malo
+// -------------------------------------------------------------------------------------------
+// FUNCIONES DE SCRIPT
+// -------------------------------------------------------------------------------------------
+
+// VALIDACION DE DATOS DEL FORMULARIO
+
 function validar($i_busqueda,$i_fecha) {
 	$validado=true;
 	$errorvalidacion="";
@@ -19,9 +23,8 @@ function validar($i_busqueda,$i_fecha) {
 	return $validado;
 }
 
-
-
 //PREPARA LA CADENA DE BUSQUEDA (ELIMINA ETIQUETAS HTML, ESPACIOS DOBLES, ETC)
+
 function preparar_cadena($cadena) {
 	$cadenatratada = trim($cadena);
 	$cadenatratada = strip_tags($cadenatratada);	
@@ -29,6 +32,7 @@ function preparar_cadena($cadena) {
 }
 
 // OBTIENE UN ARRAY CON LOS TAGS A PARTIR DE UNA CADENA DE TAGS SEPARADOS POR ESPACIOS
+
 function obtener_tags($cadena) {
 	$cadenatratada = preparar_cadena($cadena);
 	$tagssintratar = explode(" ",$cadenatratada);
@@ -42,8 +46,67 @@ function obtener_tags($cadena) {
 	return $tagstratadas;
 }
 
+// (NO DESARROLLADA) SUBRAYA LAS PALABRAS USADAS EN LA BÚSQUEDA
+
 function subrayar($cadena, $tagstratadas) {
 
 }
 
+// -------------------------------------------------------------------------------------------
+// SCRIPT DE BÚSQUEDA
+// -------------------------------------------------------------------------------------------
+
+	$query = "";
+	$cadena_busqueda=preparar_cadena($_POST["busqueda"]);
+	
+	// Establecer el patrón de búsqueda
+	
+	
+	if ( validar($_POST["busqueda"],$_POST["fecha"]) ) {
+		
+	
+		$columnas="";
+		$condiciondefecha="";
+		$condiciondelocalizacion="";
+		$query = "SELECT * FROM anuncios WHERE ";
+		
+		// Si Etiquetas está marcado añado etiquetas.  	
+		if ($_POST["tag"]) {
+			$columnas.="etiqueta,";
+		}
+		
+		// Sí título está marcado añado titulo.	
+		if ($_POST["titulo"]) {
+			$columnas.="titulo,";
+		}
+		// Sí descripcion está marcado añado descripcion
+		if ($_POST["descripcion"]) {
+			$columnas.="descripcion";
+		}
+		// PASO 4: Si hay una fecha límite de inicio añado la condición a where
+		if ($_POST["fecha"] != "" && $_POST["fecha"] != "dd-mm-aaaa") {
+			$fecha_inicial=date("Y-m-d h:i:s",strtotime($_POST["fecha"]));
+			$condiciondefecha="AND fecha_publicacion >= '$fecha_inicial'";
+		}
+		//PASO 5: Limitar la búsqueda al área de localización (No desarrollado)
+		
+		//Crea cunsulta final 
+		$columnas=rtrim($columnas,",");
+		if ($columnas == "") { $columnas="etiquetas"; }
+		$query.="MATCH ($columnas) AGAINST ('$cadena_busqueda') $condiciondefecha $condiciondelocalizacion";
+	}
+	
+		//HACER CONSULTA (NO DESARROLLADO)
+		
+		
+	// FIN DE BUSQUEDA
+	
+	$tabla=mysql_query($query);
+	if (is_array($tabla)) {					
+		while ($registro=mysql_fetch_array($tabla)) {
+			echo $registro["Name"]." - ".$registro["District"]."<br>";
+		}
+	} else {
+		echo "No hay resultados para esa búsqueda";
+	}
 ?>
